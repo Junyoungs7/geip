e079b3fbb902047d372c3fc4898de9f829e71b2d
junyoung <junyoungs7@naver.com>
2022-09-26 17:01:23 +0900

    22-09-26

--- src/main/java/com/geip/web/MainController.java
+++ src/main/java/com/geip/web/MainController.java
@@ -78,6 +78,30 @@ public class MainController {
         return "teamBuilding";
     }
 
+    @GetMapping("/recommendTB")
+    public String recommendTeamBuildingGet(MultiSearchDto multiSearchDto, Model model, @LoginUser SessionUser sessionUser){
+        MainUserInfoDto mainUserInfoDto = userService.mainUserInfoDto(sessionUser.getEmail());
+        model.addAttribute("userInfo", mainUserInfoDto);
+        model.addAttribute("multiSearchDto", multiSearchDto);
+        return "recommendTeamBuilding";
+    }
+
+    @PostMapping("/recommendTeamBuilding")
+    public String recommendTeamBuilding(@ModelAttribute("multiSearchDto") MultiSearchDto multiSearchDto, Model model, @LoginUser SessionUser sessionUser) throws JsonProcessingException {
+        String userNicknames = multiSearchDto.getGameNicknames();
+        MainUserInfoDto mainUserInfoDto = userService.mainUserInfoDto(sessionUser.getEmail());
+//        List<TeamBuildingRiotApiDTO> teamBuildingRiotApiDTOS = riotApiService.teamBuildingSearchSummoner(userNicknames);
+        RecommendTBAndControllerDTO recommendTBAndControllerDTO = riotApiService.recommendTeamBuilding(userNicknames);
+        List<RecommendTeamBuildingDto> redTeam = recommendTBAndControllerDTO.getRedTeam();
+        List<RecommendTeamBuildingDto> blueTeam = recommendTBAndControllerDTO.getBlueTeam();
+        model.addAttribute("userInfo", mainUserInfoDto);
+        model.addAttribute("redTeam", redTeam);
+        model.addAttribute("blueTeam", blueTeam);
+        return "recommendTeamBuilding";
+    }
+
+
+
 
     @GetMapping("/test")
     public String test() throws JsonProcessingException {
